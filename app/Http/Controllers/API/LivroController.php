0bb8084cbe345723e2f55ee913490ea0bcc5b4fd
Gustavo Nascimento <gustavonascimento192@gmail.com>
2022-11-25 20:40:12 -0300

    Initial Version

---
+++ app/Http/Controllers/API/LivroController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\LivroResource;
+use App\Models\Livro;
+use Illuminate\Http\Request;
+
+class LivroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $livros = Livro::all();
+        return response([ 'data' => LivroResource::collection($livros), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $livro = Livro::create($request->all());
+        return response(['data' => new LivroResource($livro), 'message' => 'Created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Livro $livro)
+    {
+        return response(['data' => new LivroResource($livro), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Livro $livro)
+    {
+        $livro->update($request->all());
+        return response(['data' => new LivroResource($livro), 'message' => 'Update successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Livro $livro)
+    {
+        $livro->delete();
+        return response(['message' => 'Deleted']);
+    }
+}
